08d8bc28c2c25a6ff800e63bc90df567421aea35
eduardogit <itenriquez.isc@gmail.com>
2017-03-18 16:07:48 -0600

    python api client last commit

--- client/Library.py
+++ client/Library.py
@@ -38,6 +38,7 @@ class Book:
 		Object = requests.get(url)
 		if(Object.json()['ok'] == False):
 			self._response.ok = "false"
+			
 		else:
 			self.title  = Object.json()['item']['book']['title']
 			self.author = Object.json()['item']['book']['author']
